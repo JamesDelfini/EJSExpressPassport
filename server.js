32fdafd57a13c8740aa147769f802b25ecd5b0ea
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-19 17:12:18 +0800

    Setup EJS Routes and Views in Express

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const { render } = require('ejs');
 const app = express();
 
+const users = [];
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false })) // Take forms and access them in req variable in POST HTTP Methods
+
 app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Delfini' });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', (req, res) => {
 
 });
 
